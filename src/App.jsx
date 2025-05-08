763a6799ba013e1575f1802682bd5f5845bf2ebd
scribblenovice <pasomba41@gmail.com>
2025-05-08 23:47:47 +0100

    major revamping of everything

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Loader from "./globalcomponents/Loader";
 import ScrollToTop from "./globalcomponents/ScrollToTop";
 import WhatsappBtn from "./globalcomponents/WhatsappBtn";
 import BackToTopButton from "./globalcomponents/BackToTop";
+
 const HomePage = lazy(() => import("./pages/Homepage"));
 const Solar = lazy(() => import("./pages/solar-installation/Solar"));
 const Access = lazy(() => import("./pages/access-control/Access"));
 const Contact = lazy(() => import("./pages/contact/Contact"));
 const OurServices = lazy(() => import("./pages/Services/OurServices"));
-
 const Error404Page = lazy(() => import("./pages/Error404Page"));
 const IctServices = lazy(() => import("./pages/ict/IctServices"));
 const TechConsulting = lazy(() => import("./pages/consulting/TechConsulting"));
@@ -23,86 +23,22 @@ function App() {
       <WhatsappBtn />
       <BackToTopButton />
       <ScrollToTop />
-      <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <Suspense fallback={<Loader />}>
-                <HomePage />
-              </Suspense>
-            }
-          />
-
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="services">
-            <Route
-              index
-              element={
-                <Suspense fallback={<Loader />}>
-                  <OurServices />
-                </Suspense>
-              }
-            />
-            <Route
-              path="solar-installation"
-              element={
-                <Suspense fallback={<Loader />}>
-                  <Solar />
-                </Suspense>
-              }
-            />
-            <Route
-              path="ict-solutions"
-              element={
-                <Suspense fallback={<Loader />}>
-                  <IctServices />
-                </Suspense>
-              }
-            />
-            <Route
-              path="cctv-and-access-control"
-              element={
-                <Suspense fallback={<Loader />}>
-                  <Access />
-                </Suspense>
-              }
-            />
-            <Route
-              path="tech-consulting"
-              element={
-                <Suspense fallback={<Loader />}>
-                  <TechConsulting />
-                </Suspense>
-              }
-            />
+            <Route index element={<OurServices />} />
+            <Route path="solar-installation" element={<Solar />} />
+            <Route path="ict-solutions" element={<IctServices />} />
+            <Route path="cctv-and-access-control" element={<Access />} />
+            <Route path="tech-consulting" element={<TechConsulting />} />
           </Route>
-          <Route
-            path="contact-us"
-            element={
-              <Suspense fallback={<Loader />}>
-                <Contact />
-              </Suspense>
-            }
-          />
-          <Route
-            path="gallery"
-            element={
-              <Suspense fallback={<Loader />}>
-                <GalleryPage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="power-audit"
-            element={
-              <Suspense fallback={<Loader />}>
-                <PowerAudit />
-              </Suspense>
-            }
-          />
+          <Route path="contact-us" element={<Contact />} />
+          <Route path="gallery" element={<GalleryPage />} />
+          <Route path="power-audit" element={<PowerAudit />} />
           <Route path="*" element={<Error404Page />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 }
--- src/globalcomponents/Loader.jsx
+++ src/globalcomponents/Loader.jsx
@@ -2,7 +2,7 @@ import src from "../assets/logo.png";
 const Loader = () => {
   return (
     <div className="bg-white h-screen w-screen flex justify-center items-center">
-      <img src={src} alt="" className="animate-bounce w-24 h-auto opacity-80" />
+      <img src={src} alt="" className="animate-bounce w-16 md:w-20 lg:w-24 h-auto opacity-80" />
     </div>
   );
 };
